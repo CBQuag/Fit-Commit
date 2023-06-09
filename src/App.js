76f036055122e5818aaab8969580e2300bcce02c
Conrad Quagliaroli <conradbquag@gmail.com>
2023-06-09 08:57:21 -0400

    small mistake

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
   
   function datediff(first, second) {
     let fDate = new Date(first)
-    let sDate = new Date(first)
+    let sDate = new Date(second)
     let cleanFDate = new Date(fDate.getFullYear(), fDate.getMonth(), fDate.getDate())
     let cleanSDate=new Date(sDate.getFullYear(), sDate.getMonth(), sDate.getDate())
     return Math.round((cleanSDate - cleanFDate.getTime()) / (1000 * 60 * 60 * 24) );
